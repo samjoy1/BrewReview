03c00ceed8518a14a4ad40300ee1ecb4366e9da4
Nicole Raymond <nicoleraymond@hotmail.com>
2025-06-02 17:03:13 +0100

    adds users list

--- app/src/components/pages/Users.jsx
+++ app/src/components/pages/Users.jsx
@@ -1,6 +1,31 @@
+import { collection, getDocs } from "firebase/firestore";
+import { FIRESTORE_DB } from "../../../../firebaseconfig";
+import { useState, useEffect } from "react";
 
-function Users () {
+function Users() {
+  const [users, setUsers] = useState([]);
 
+  useEffect(() => {
+    getDocs(collection(FIRESTORE_DB, "users"))
+      .then((snapshot) => {
+        const usersArray = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setUsers(usersArray);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          <p>ID:</p> {user.id}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-export default Users
+export default Users;
