92f645166084c6e1db87fc132bef11c2976be6ae
Sam Joy <samjoy@Sams-Mac-mini.local>
2025-06-02 15:04:13 +0100

    added naviagtion

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,32 @@
 import { Text, View } from "react-native";
-import ListOfBeers from "./ListOfBeers";
 import React from "react";
 import "@/global.css";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createNativeStackNavigator();
 
 // components
 import { Beer, Brewery, Camera, Categories, Home, Login, Map, PostBeer, PostReview, Profile, Search, Settings, User, Users } from "./src/components/pages/Componentsindex"
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-red-300">
-      <Beer/>
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Beer" component={Beer} />
+      <Stack.Screen name="Brewery" component={Brewery} />
+      <Stack.Screen name="Camera" component={Camera} />
+      <Stack.Screen name="Categories" component={Categories} />
+
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Map" component={Map} />
+      <Stack.Screen name="PostBeer" component={PostBeer} />
+      <Stack.Screen name="PostReview" component={PostReview} />
+      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="Search" component={Search} />
+      <Stack.Screen name="Settings" component={Settings} />
+      <Stack.Screen name="User" component={User} />
+      <Stack.Screen name="Users" component={Users} />
+    </Stack.Navigator>
   );
 }
--- app/src/components/pages/Home.jsx
+++ app/src/components/pages/Home.jsx
@@ -1,6 +1,14 @@
+import { Button } from "@react-navigation/elements";
+import { Text, View } from "react-native";
 
-function Home () {
-    return
+function Home({navigation}) {
+  return (
+    <View>
+      <Button onPress={() => {navigation.navigate('Beer')}}>
+        <Text>Goodbye</Text>
+      </Button>
+    </View>
+  );
 }
 
-export default Home
+export default Home;
