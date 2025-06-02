2ede67a1510ec6474861b46291fefcf4ebb2ef55
Nicole Raymond <nicoleraymond@hotmail.com>
2025-06-02 17:05:34 +0100

    adds individual user

--- app/src/components/pages/User.jsx
+++ app/src/components/pages/User.jsx
@@ -1,6 +1,33 @@
+"use client";
 
-function User () {
+import { doc, getDoc } from "firebase/firestore";
+import { FIRESTORE_DB } from "../../../../firebaseconfig";
+import { useState, useEffect } from "react";
 
+function User({ userId }) {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    getDoc(doc(FIRESTORE_DB, "users", userId))
+      .then((docSnap) => {
+        if (docSnap.exists()) {
+          setUser({ id: docSnap.id, ...docSnap.data() });
+        }
+      })
+      .catch((err) => console.error(err));
+  }, [userId]);
+
+  return (
+    <div>
+      {user && (
+        <>
+          <p>ID: {user.id}</p>
+          {/* <p>Name: {user.name}</p> */}
+          {/* <p>Email: {user.email}</p> */}
+        </>
+      )}
+    </div>
+  );
 }
 
-export default User
+export default User;
