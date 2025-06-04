12799061c159346a4b8271f67b29a020b89820b7
Morgan Hewitt <morganhewitt0211@gmail.com>
2025-06-04 16:08:46 +0200

    adding some styling

--- app/src/components/pages/Settings.jsx
+++ app/src/components/pages/Settings.jsx
@@ -1,34 +1,28 @@
 import React from "react";
-import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { ScrollView, Text, TouchableOpacity } from "react-native";
 
 function Settings() {
   return (
     <ScrollView>
-      <View>
-        <TouchableOpacity>
-          <Text>Change Username</Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <TouchableOpacity>
-          <Text>Change Password</Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <TouchableOpacity>
-          <Text>Change Profile Picture</Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <TouchableOpacity>
-          <Text>Dark Mode</Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <TouchableOpacity>
-          <Text>About</Text>
-        </TouchableOpacity>
-      </View>
+      <TouchableOpacity className="border bg-white border-gray-300 rounded-lg p-3">
+        <Text className="text-lg font-semibold">Change Username</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity className="border bg-white border-gray-300 rounded-lg p-3">
+        <Text className="text-lg font-semibold">Change Password</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity className="border bg-white border-gray-300 rounded-lg p-3">
+        <Text className="text-lg font-semibold">Change Profile Picture</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity className="border bg-white border-gray-300 rounded-lg p-3">
+        <Text className="text-lg font-semibold">Dark Mode</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity className="border bg-white border-gray-300 rounded-lg p-3">
+        <Text className="text-lg font-semibold">About</Text>
+      </TouchableOpacity>
     </ScrollView>
   );
 }
