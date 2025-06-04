a54314428f36ef10f0e5228ecf20dbd893f6f11d
Morgan Hewitt <morganhewitt0211@gmail.com>
2025-06-04 15:14:30 +0200

    creating and planning settings

--- app/src/components/pages/Settings.jsx
+++ app/src/components/pages/Settings.jsx
@@ -1,6 +1,36 @@
+import React from "react";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 
-function Settings () {
-
+function Settings() {
+  return (
+    <ScrollView>
+      <View>
+        <TouchableOpacity>
+          <Text>Change Username</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity>
+          <Text>Change Password</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity>
+          <Text>Change Profile Picture</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity>
+          <Text>Dark Mode</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity>
+          <Text>About</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
 }
 
-export default Settings
+export default Settings;
