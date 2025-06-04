6f8ff4c983c55fc710bef9c6d8e7718f5b4277c8
Morgan Hewitt <morganhewitt0211@gmail.com>
2025-06-04 13:18:12 +0200

    BR-15 finalising things and correcting imports and exports before making a pull request

--- app/src/components/pages/Beer.jsx
+++ app/src/components/pages/Beer.jsx
@@ -10,7 +10,7 @@ import {
   Header,
   InfoButtons,
   ShareButton,
-} from "../beer/index";
+} from "../beer/Index";
 
 // TO DO BUT NEED TO WAIT
 // - dynamically get the required information from the database
@@ -20,9 +20,8 @@ import {
 // - toast being a global thing we can use
 
 function Beer() {
-    
   // placeholder data, to be replaced once databases have all required info
-  const name = "Awesome Brew";
+  const name = "Tasty Bev";
   const image =
     "https://imgs.search.brave.com/HTnfzB4GPTeNE42Sm6aAH116T7QcNedDW2gE4mTiaks/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNTQ1/ODY0NTU5L3Bob3Rv/L3VzYS1uZXctamVy/c2V5LWhhbmQtcG91/cmluZy1iZWVyLmpw/Zz9zPTYxMng2MTIm/dz0wJms9MjAmYz1F/Y0R2MXRqbjM1eEJt/amtUR0dkMmRTYk9P/eWZ1U0dTSWhlNUtM/bE5xSjFVPQ";
   const type = "IPA";
