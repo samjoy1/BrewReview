c2e1fe30103a6d9c9b45b8f01385d15a37d7fda9
gmharper <gmharper@BOOK-I42E130TL8.localdomain>
2025-06-05 17:10:30 +0100

    Created useContext in index.jsx with isLoggedIn, loggedInUser & theme states

--- app/index.jsx
+++ app/index.jsx
@@ -1,43 +1,43 @@
-import "@/global.css";
+// imports
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React from "react";
-const Stack = createNativeStackNavigator();
+import { createContext, useState } from "react";
+
+// styling
+import "@/global.css";
 
 // components
 import {
-  Beer,
-  Brewery,
-  Camera,
-  Categories,
-  Home,
-  Login,
-  Map,
-  PostBeer,
-  PostReview,
-  Profile,
-  Search,
-  Settings,
-  User,
-  Users,
+  Beer, Brewery, Camera, Categories, Home, Login, Map, PostBeer, PostReview, Profile, Search, Settings, User, Users,
 } from "./src/components/pages/Componentsindex";
 
+// variables
+export const UserContext = createContext(null)
+const Stack = createNativeStackNavigator();
+
 export default function Index() {
+  // useStates
+  const [isLoggedIn, setIsLoggedIn] = useState(true)
+  const [loggedInUser, setLoggedInUser] = useState("brewcat108")
+  const [theme, setTheme] = useState("light")
+
   return (
-    <Stack.Navigator initialRouteName="Beer">
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Beer" component={Beer} />
-      <Stack.Screen name="Brewery" component={Brewery} />
-      <Stack.Screen name="Camera" component={Camera} />
-      <Stack.Screen name="Categories" component={Categories} />
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Map" component={Map} />
-      <Stack.Screen name="PostBeer" component={PostBeer} />
-      <Stack.Screen name="PostReview" component={PostReview} />
-      <Stack.Screen name="Profile" component={Profile} />
-      <Stack.Screen name="Search" component={Search} />
-      <Stack.Screen name="Settings" component={Settings} />
-      <Stack.Screen name="User" component={User} />
-      <Stack.Screen name="Users" component={Users} />
-    </Stack.Navigator>
+    <UserContext.Provider value={{isLoggedIn, setIsLoggedIn, loggedInUser, setLoggedInUser, theme, setTheme}}>
+      <Stack.Navigator initialRouteName="PostReview">
+        <Stack.Screen name="PostReview" component={PostReview} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Beer" component={Beer} />
+        <Stack.Screen name="Brewery" component={Brewery} />
+        <Stack.Screen name="Camera" component={Camera} />
+        <Stack.Screen name="Categories" component={Categories} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Map" component={Map} />
+        <Stack.Screen name="PostBeer" component={PostBeer} />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Search" component={Search} />
+        <Stack.Screen name="Settings" component={Settings} />
+        <Stack.Screen name="User" component={User} />
+        <Stack.Screen name="Users" component={Users} />
+        </Stack.Navigator>
+    </UserContext.Provider>
   );
 }
