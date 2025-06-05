43302fafdf2d675ce8393c50b2cecfbe2c02f72e
gmharper <gmharper@BOOK-I42E130TL8.localdomain>
2025-06-05 15:16:07 +0100

    Created basic post.js script file

---
+++ app/scripts/post.js
@@ -0,0 +1,19 @@
+import { hasUserReviewed, hasUserVoted } from "../utilities";
+
+function postReview (username, beer_id) { // username: the username of the user leaving the review | beer_id: the id of the beer the review is for
+    if (!hasUserReviewed(username, beer_id)) {
+
+    }
+}
+
+function postBeer () {
+
+}
+
+function createNewUser () {
+
+}
+
+function createNewCategory () {
+    
+}
