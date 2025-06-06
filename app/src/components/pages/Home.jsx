956189846d892552fd5f0c6cba6643eb1cf78101
manuelgcl25 <mgcaldasl@gmail.com>
2025-06-06 12:05:00 +0100

    Bring back navigation routing + bring back folders/file structure + implement the basis for when we have authentication in place we require users to be authenticated. Until then, we use a dummy default user

--- app/src/components/pages/Home.jsx
+++ app/src/components/pages/Home.jsx
@@ -1,18 +1,21 @@
-import { useRouter } from "expo-router";
+import { getAuth } from "firebase/auth";
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
-import Navbar from "./src/components/pages/NavBar";
+import Navbar from "./NavBar";
 
-export default function Home() {
-  const router = useRouter();
+export default function Home({ navigation }) {
+  const DUMMY_USER_ID = "bigdog512";
+  const auth = getAuth();
+  const currentUser = auth.currentUser;
 
   return (
     <View className="flex-1 bg-white">
       {/*This Profile button below is just for the time being since the one in the HeaderNav is not rendering */}
       <TouchableOpacity
-        // thhis line below, need updating the query params once we have authentication to have the
-        // the userID from the signed in user
-        onPress={() => router.push("/Profile?userId=bigdog512")}
+        onPress={() => {
+          const userId = currentUser?.userId || DUMMY_USER_ID;
+          navigation.navigate("Profile", { userId });
+        }}
         style={{ padding: 10, backgroundColor: "#eee", margin: 10 }}
       >
         <Text>Go to Profile</Text>
