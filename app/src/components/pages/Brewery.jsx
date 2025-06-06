a121949927697c8a1525da51ec136b893b1938e5
Morgan Hewitt <morganhewitt0211@gmail.com>
2025-06-06 12:44:03 +0200

    adding buttons and data

--- app/src/components/pages/Brewery.jsx
+++ app/src/components/pages/Brewery.jsx
@@ -3,8 +3,9 @@ import { useEffect, useState } from "react";
 import { ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { getBreweryById } from "../../../utilities";
-import IndividualBeerHeader from "../beer/IndividualBeerHeader";
 import BreweryImage from "../brewery/BreweryImage";
+import BreweryInfoButtons from "../brewery/BreweryInfoButtons";
+import IndividualBreweryHeader from "../brewery/IndividualBreweryHeader";
 import Header from "./HeaderNav";
 import Navbar from "./NavBar";
 
@@ -37,10 +38,33 @@ function Brewery() {
   function handlePressHeartButton() {
     setLiked(!liked);
   }
+  // should go to all beers filtered by country
+  function handlePressCountry() {
+    navigation.navigate("Search");
+  }
+
+  // go to the url provided
+  function handlePressUrl() {
+    navigation.navigate("Search");
+  }
+
+  // go to beers filtered by brewery selected
+  function handlePressBeers() {
+    navigation.navigate("Search");
+  }
+
+  function handlePressCity() {
+    // do something nice and cool, dno yet
+    navigation.navigate("Search");
+  }
 
   // USING THE BREWERY DATA
   const name = breweryData?.name || "Loading";
   const image = breweryData?.img_url;
+  const country = breweryData?.country;
+  const city = breweryData?.location;
+  const url = breweryData?.url;
+  const foundedDate = breweryData?.founded_date;
 
   return (
     <SafeAreaView className="flex-1 bg-gray-100">
@@ -50,12 +74,24 @@ function Brewery() {
         contentContainerStyle={{ paddingBottom: 100 }}
         showsVerticalScrollIndicator={false}
       >
-        <IndividualBeerHeader
+        <IndividualBreweryHeader
           name={name}
           liked={liked}
           onHeartButtonPress={handlePressHeartButton}
         />
         <BreweryImage image={image} />
+
+        <BreweryInfoButtons
+          country={country}
+          city={city}
+          url={url}
+          foundedDate={foundedDate}
+          onBeersButtonPress={handlePressBeers}
+          onUrlButtonPress={handlePressUrl}
+          onCountyButtonPress={handlePressCountry}
+          onCityButtonPress={handlePressCity}
+          name={name}
+        />
       </ScrollView>
       <Navbar />
     </SafeAreaView>
