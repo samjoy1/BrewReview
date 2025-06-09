5796c79aa990c1ab873545747f49f83cef4dd2e2
gmharper <gmharper@BOOK-I42E130TL8.localdomain>
2025-06-09 18:11:54 +0100

    Post Beer page

--- app/src/components/pages/PostBeer.jsx
+++ app/src/components/pages/PostBeer.jsx
@@ -1,6 +1,95 @@
+// IMPORTS
+import { useContext, useEffect, useState } from "react";
+import { ImageBackground, ScrollView, Text, View } from "react-native-web";
+import Toast from 'react-native-toast-message';
+
+import { UserContext } from "../../../index.jsx";
+// SCRIPTS
+import { postBeer } from "../../../scripts/post";
+
+// COMPONENTS
+import HeaverNav from "../pages/HeaderNav";
+import NavBar from "../pages/NavBar";
+import BeerForm from "../postBeer/BeerForm.jsx";
+
 
 function PostBeer () {
+    // STATES
+    const { loggedInUser } = useContext(UserContext)
 
+    const [beer, setBeer] = useState({
+        id: "",
+        name: "",
+        country: "",
+        category: "",
+        brewery: "",
+        logo_url: "",
+        img_url: "",
+        percentage: 0,
+        tags: [],
+        reviews: [],
+    })
+
+    // VALIDATION
+    function isBeerValid (newBeer) {
+        if (!newBeer.name) {
+            Toast.show({
+                type: "error",
+                text1: "Please enter a name for your beer"
+            })
+            return false
+        }
+        if (!newBeer.country) {
+            Toast.show({
+                type: "error",
+                text1: "Please enter the country your beer is from"
+            })
+            return false
+        }
+        if (!newBeer.category) {
+            Toast.show({
+                type: "error",
+                text1: "Please select a category"
+            })
+            return false
+        }
+        return true
+    }
+
+    // SUBMIT
+    function submitBeer (newBeer) {
+        setBeer(newBeer)
+        if (isBeerValid(newBeer)) {
+            postBeer(newBeer)
+            Toast.show({
+                type: "success",
+                text1: "You just created a new Beer! Why not give it a rating?"
+            })
+        }
+        // should reload the page
+    }
+        
+
+    useEffect(() => {
+    }, [])
+
+    return (
+        <ScrollView className="relative flex-1">
+            <ImageBackground source={ require("../../../../assets/images/BR-bg.png") }
+                    className="relative flex-shrink p-2">
+                <HeaverNav className=""/>
+
+                <View className="p-6">
+                    <Text className="bg-zinc-800/90 rounded-xl color-white font-bold h-full text-2xl mb-2 mr-32 p-3"> Beer doesn't exist yet? Create one!</Text>
+                    <BeerForm submitBeer={submitBeer}/>
+                </View>
+            </ImageBackground>
+            <NavBar />
+        </ScrollView>
+    )
 }
 
 export default PostBeer
+
+
+
---
+++ app/src/components/postBeer/SelectCategory.jsx
@@ -0,0 +1,213 @@
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from 'react';
+import { FIRESTORE_DB } from '../../../../firebaseconfig.js';
+
+import { StyleSheet, Text, View } from 'react-native';
+import SelectDropdown from 'react-native-select-dropdown';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+function SelectCategory ({ setCategory }) {
+    const [categories, setCategories] = useState([{}])
+    
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    async function getCategories () {
+        setLoading(true)
+        
+        await getDocs(collection(FIRESTORE_DB, "categories"))
+        .then((snapshot) => {
+            let categoriesArray = []
+            snapshot.forEach((doc) => {
+                categoriesArray.push(doc.data())
+            })
+            setCategories(categoriesArray)
+        })
+        .catch((err) => { setError(err) })
+        .finally(() => { setLoading(false) })
+    }
+
+    useEffect(() => {
+        getCategories()
+    }, [])
+
+  return (
+    <View className="flex-row m-2">
+      <SelectDropdown
+        data={categories}
+        // defaultValueByIndex={8} // use default value by index or default value
+        // defaultValue={{title: 'kiss', icon: 'emoticon-kiss-outline'}} // use default value by index or default value
+        onSelect={(selectedItem, index) => {
+          setCategory(selectedItem)
+        }}
+        renderButton={(selectedItem, isOpen) => {
+          return (
+            <View className="flex-row bg-gray-700 rounded-xl py-2 w-full">
+              {/* <Icon name={selectedItem ? selectedItem.icon : 'emoticon'} style={styles.dropdown1ButtonIconStyle} /> */}
+              <Text style={styles.dropdown1ButtonTxtStyle}>
+                { selectedItem ? (selectedItem.name) : 'Select a Category'}
+              </Text>
+              <Icon name={isOpen ? 'chevron-up' : 'chevron-down'} style={styles.dropdown1ButtonArrowStyle} />
+            </View>
+          );
+        }}
+        renderItem={(item, index, isSelected) => {
+          return (
+            loading ? <View><Text>Loading Categories...</Text></View> :
+                <View
+                style={{
+                    ...styles.dropdown1ItemStyle,
+                    ...(isSelected && {backgroundColor: 'grey'}),
+                }}>
+                    <Text style={styles.dropdown1ItemTxtStyle}>{item.name}</Text>
+                    <Text style={styles.dropdown1ItemTxtStyle}>{item.category}</Text>
+                </View> 
+          );
+        }}
+        dropdownStyle={styles.dropdown1MenuStyle}
+        showsVerticalScrollIndicator={false}
+        search
+        searchInputStyle={styles.dropdown1SearchInputStyle}
+        searchInputTxtColor={'#FFFFFF'}
+        searchPlaceHolder={'  Search beers'}
+        searchPlaceHolderColor={'#F8F8F8'}
+        renderSearchInputLeftIcon={() => {
+          return <FontAwesome name={'search'} color={'#F8F8F8'} size={18} />;
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: 50,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingTop: 116,
+  },
+  header: {
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+    height: 90,
+    backgroundColor: '#E9ECEF',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    paddingBottom: 16,
+  },
+  headerTxt: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#151E26',
+  },
+  dropdownButtonStyle: {
+    width: 350,
+    height: 50,
+    backgroundColor: '#E9ECEF',
+    borderRadius: 12,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 12,
+  },
+  dropdownButtonTxtStyle: {
+    flex: 1,
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#151E26',
+    textAlign: 'center',
+  },
+  dropdownMenuStyle: {
+    backgroundColor: '#E9ECEF',
+    borderRadius: 8,
+  },
+  dropdownSearchInputStyle: {
+    backgroundColor: '#E9ECEF',
+    borderRadius: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: '#B1BDC8',
+  },
+  dropdownItemStyle: {
+    width: '100%',
+    flexDirection: 'row',
+    paddingHorizontal: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: '#B1BDC8',
+  },
+  dropdownItemTxtStyle: {
+    flex: 1,
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#151E26',
+    textAlign: 'center',
+  },
+  dropdownItemIconStyle: {
+    fontSize: 28,
+    marginRight: 8,
+  },
+  ////////////// dropdown1
+  dropdown1ButtonStyle: {
+    width: '80%',
+    height: 50,
+    borderRadius: 12,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 12,
+    backgroundColor: '#444444',
+  },
+  dropdown1ButtonTxtStyle: {
+    flex: 1,
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#FFFFFF',
+    textAlign: 'center',
+  },
+  dropdown1ButtonArrowStyle: {
+    fontSize: 28,
+    color: '#FFFFFF',
+  },
+  dropdown1ButtonIconStyle: {
+    fontSize: 28,
+    marginRight: 8,
+    color: '#FFFFFF',
+  },
+  dropdown1MenuStyle: {
+    backgroundColor: '#444444',
+    borderRadius: 8,
+  },
+  dropdown1SearchInputStyle: {
+    backgroundColor: '#444444',
+    borderBottomWidth: 1,
+    borderBottomColor: '#FFFFFF',
+  },
+  dropdown1ItemStyle: {
+    width: '100%',
+    flexDirection: 'row',
+    paddingHorizontal: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: '#B1BDC8',
+  },
+  dropdown1ItemTxtStyle: {
+    flex: 1,
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#FFFFFF',
+  },
+  dropdown1ItemIconStyle: {
+    fontSize: 28,
+    marginRight: 8,
+    color: '#FFFFFF',
+  },
+});
+
+export default SelectCategory
