107023a9aba91e33d0bbfabcefde8ced179af575
Morgan Hewitt <morganhewitt0211@gmail.com>
2025-06-11 17:52:10 +0200

    debugging and formatting for mobile

--- app/src/components/pages/PostBeer.jsx
+++ app/src/components/pages/PostBeer.jsx
@@ -1,7 +1,14 @@
 // IMPORTS
 import { useContext, useEffect, useState } from "react";
-import Toast from 'react-native-toast-message';
-import { ImageBackground, ScrollView, Text, View } from "react-native-web";
+import {
+  Dimensions,
+  ImageBackground,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
+import Toast from "react-native-toast-message";
 
 import { UserContext } from "../../../index.jsx";
 // SCRIPTS
@@ -12,84 +19,97 @@ import HeaverNav from "../pages/HeaderNav";
 import NavBar from "../pages/NavBar";
 import BeerForm from "../postBeer/BeerForm.jsx";
 
+const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
 
-function PostBeer () {
-    // STATES
-    const { loggedInUser } = useContext(UserContext)
+function PostBeer() {
+  // STATES
+  const { loggedInUser } = useContext(UserContext);
 
-    const [beer, setBeer] = useState({
-        id: "",
-        name: "",
-        country: "",
-        category: "",
-        brewery: "",
-        logo_url: "",
-        img_url: "",
-        percentage: 0,
-        tags: [],
-        reviews: [],
-    })
+  const [beer, setBeer] = useState({
+    id: "",
+    name: "",
+    country: "",
+    category: "",
+    brewery: "",
+    logo_url: "",
+    img_url: "",
+    percentage: 0,
+    tags: [],
+    reviews: [],
+  });
 
-    // VALIDATION
-    function isBeerValid (newBeer) {
-        if (!newBeer.name) {
-            Toast.show({
-                type: "error",
-                text1: "Please enter a name for your beer"
-            })
-            return false
-        }
-        if (!newBeer.country) {
-            Toast.show({
-                type: "error",
-                text1: "Please enter the country your beer is from"
-            })
-            return false
-        }
-        if (!newBeer.category) {
-            Toast.show({
-                type: "error",
-                text1: "Please select a category"
-            })
-            return false
-        }
-        return true
+  // VALIDATION
+  function isBeerValid(newBeer) {
+    if (!newBeer.name) {
+      Toast.show({
+        type: "error",
+        text1: "Please enter a name for your beer",
+      });
+      return false;
     }
-
-    // SUBMIT
-    function submitBeer (newBeer) {
-        setBeer(newBeer)
-        if (isBeerValid(newBeer)) {
-            postBeer(newBeer)
-            Toast.show({
-                type: "success",
-                text1: "You just created a new Beer! Why not give it a rating?"
-            })
-        }
-        // should reload the page
+    if (!newBeer.country) {
+      Toast.show({
+        type: "error",
+        text1: "Please enter the country your beer is from",
+      });
+      return false;
     }
-        
+    if (!newBeer.category) {
+      Toast.show({
+        type: "error",
+        text1: "Please select a category",
+      });
+      return false;
+    }
+    return true;
+  }
 
-    useEffect(() => {
-    }, [])
+  // SUBMIT
+  function submitBeer(newBeer) {
+    setBeer(newBeer);
+    if (isBeerValid(newBeer)) {
+      postBeer(newBeer);
+      Toast.show({
+        type: "success",
+        text1: "You just created a new Beer! Why not give it a rating?",
+      });
+    }
+    // should reload the page
+  }
 
-    return (
-        <ScrollView className="relative flex-1">
-            <ImageBackground source={ require("../../../../assets/images/BR-bg.png") }
-                    className="relative flex-shrink p-2">
-                <HeaverNav className=""/>
+  useEffect(() => {}, []);
 
-                <View className="p-6">
-                    <Text className="bg-zinc-800/90 rounded-xl color-white font-bold h-full text-2xl mb-2 mr-32 p-3"> Beer doesn't exist yet? Create one!</Text>
-                    <BeerForm submitBeer={submitBeer}/>
-                </View>
-            </ImageBackground>
-            <NavBar />
-        </ScrollView>
-    )
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      className="relative flex-1"
+    >
+      <SafeAreaView style={{ flex: 1 }}>
+        <ImageBackground
+          source={require("../../../../assets/images/BR-bg.png")}
+          style={{ flex: 1, width: "100%", height: "100%" }}
+          className="relative flex-shrink p-2"
+          resizeMode="cover"
+        >
+          <HeaverNav />
+          <View className="p-6" style={{ flex: 1 }}>
+            <Text
+              className="bg-zinc-800/90 rounded-xl color-white font-bold text-2xl mb-2 p-3"
+              style={{
+                width: screenWidth * 0.8,
+                alignSelf: "center",
+                textAlign: "center",
+              }}
+            >
+              Beer doesn't exist yet? Create one!
+            </Text>
+            <BeerForm submitBeer={submitBeer} />
+          </View>
+        </ImageBackground>
+      </SafeAreaView>
+      <NavBar />
+    </ScrollView>
+  );
 }
 
-export default PostBeer
-
-
-
+export default PostBeer;
--- app/src/components/postBeer/BeerForm.jsx
+++ app/src/components/postBeer/BeerForm.jsx
@@ -1,184 +1,276 @@
 // IMPORTS
 import { useEffect, useState } from "react";
-import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  Dimensions,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 // COMPONENTS
 import { BeerCardTextBox, SelectBrewery, SelectCategory } from "./index.jsx";
 
 // TAILWIND STYLING
-let text_box = "text-white bg-zinc-800 m-1 p-1 rounded-xl h-7 align-middle"
-let text_input = "bg-white rounded-xl m-2 p-2 min-h-9"
+let text_box = "text-white bg-zinc-800 m-1 p-1 rounded-xl h-7 align-middle";
+let text_input = "bg-white rounded-xl m-2 p-2 min-h-9";
 
-function BeerForm ({ submitBeer }) {
-    const [brewery, setBrewery] = useState({})
-    const [category, setCategory] = useState({})
+const { width: screenWidth } = Dimensions.get("window");
 
-    const [nameInput, setNameInput] = useState("")
-    const [countryInput, setCountryInput] = useState("")
-    const [percentageInput, setPercentageInput] = useState(0)
-    const [logoInput, setLogoInput] = useState("")
-    const [imageInput, setImageInput] = useState("")
-    const [tags, setTags] = useState([])
+function BeerForm({ submitBeer }) {
+  const [brewery, setBrewery] = useState({});
+  const [category, setCategory] = useState({});
 
-    const [titleError, setTitleError] = useState(false)
-    const [bodyError, setBodyError] = useState(false)
+  const [nameInput, setNameInput] = useState("");
+  const [countryInput, setCountryInput] = useState("");
+  const [percentageInput, setPercentageInput] = useState(0);
+  const [logoInput, setLogoInput] = useState("");
+  const [imageInput, setImageInput] = useState("");
+  const [tags, setTags] = useState([]);
 
-    // SET FUNCTIONS
-    function handleInput (event, inputType) {
-        switch (inputType) {
-            case "name":
-                setNameInput(event)
-                break
-            case "country":
-                setCountryInput(event)
-                break
-            case "logo":
-                setLogoInput(event)
-                break
-            case "image":
-                setImageInput(event)
-                break
-            case "percentage":
-                setPercentageInput(event)
-                break
-            case "tags":
-                setTags(event)
-                break
-        }
+  const [titleError, setTitleError] = useState(false);
+  const [bodyError, setBodyError] = useState(false);
+
+  // SET FUNCTIONS
+  function handleInput(event, inputType) {
+    switch (inputType) {
+      case "name":
+        setNameInput(event);
+        break;
+      case "country":
+        setCountryInput(event);
+        break;
+      case "logo":
+        setLogoInput(event);
+        break;
+      case "image":
+        setImageInput(event);
+        break;
+      case "percentage":
+        setPercentageInput(event);
+        break;
+      case "tags":
+        setTags(event);
+        break;
     }
+  }
 
-    // UTILITY
-    function nameToId (beerName) {
-        let modifiedName = beerName.replaceAll(" ", "_")
-        return modifiedName.toLowerCase()
+  // UTILITY
+  function nameToId(beerName) {
+    let modifiedName = beerName.replaceAll(" ", "_");
+    return modifiedName.toLowerCase();
+  }
+
+  function createBeer() {
+    let beer_id = nameToId(nameInput);
+
+    const newBeer = {
+      id: beer_id,
+      name: nameInput,
+      country: countryInput,
+      category: category.id,
+      brewery: brewery.id,
+      logo_url: logoInput,
+      img_url: imageInput,
+      percentage: percentageInput,
+      tags: [],
+      reviews: [],
+    };
+    if (!nameInput) {
+      setTitleError(true);
     }
+    submitBeer(newBeer);
+  }
 
-    function createBeer () {
-        let beer_id = nameToId(nameInput)
+  useEffect(() => {}, []);
 
-        const newBeer = {
-        id: beer_id,
-        name: nameInput,
-        country: countryInput,
-        category: category.id,
-        brewery: brewery.id,
-        logo_url: logoInput,
-        img_url: imageInput,
-        percentage: percentageInput,
-        tags: [],
-        reviews: [],
-    }
-        if (!nameInput) {
-            setTitleError(true)
-        }
-        submitBeer(newBeer)
-    }
+  return (
+    <View style={styles.container}>
+      {/* BEER CARD PREVIEW */}
+      <View className="text-white w-32 bg-stone-900 rounded-t-xl ml-6 p-2">
+        <Text>Preview</Text>
+      </View>
 
-    useEffect(() => {
-    }, [])
-
-    return (
-        
-        <View>
-            {/* BEER CARD */}
-            <View className="text-white w-32 bg-stone-900 rounded-t-xl ml-6 p-2">Preview</View>
-            <View className="flex-row h-58 w-full bg-yellow-900/90 rounded-xl border border-amber-300/90 mb-6 p-4 shadow-lg">
-                
-                <View className="flex-1">
-                    <BeerCardTextBox text={nameInput} placeholder={"Name"} />
-                    <BeerCardTextBox text={category.name} placeholder={"Category"} />
-                    <BeerCardTextBox text={brewery.name} placeholder={"Brewery"} />
-                    <BeerCardTextBox text={countryInput} placeholder={"Country"} />
-                    <BeerCardTextBox text={"tags"} placeholder={"Tags"} />
-                </View>
-                <View className="flex-1">
-                    <Image source={require("../../../../assets/images/default-beer-image.png")}
-                    style={styles.beer_img} className="" />
-                </View>
-            </View>
-
-            {/* BEER FORM */}
-            <View className="text-white w-32 bg-stone-900 rounded-t-xl ml-6 p-2">Create a Beer</View>
-            <View className="w-full bg-stone-900/90 rounded-xl p-8 border border-amber-300/90 shadow-lg">
-                <TextInput className={text_input+" min-h-9"}                           // set name
-                    placeholder="Enter the name of your beer..."
-                    onChangeText={(event) => {handleInput(event, "name")}} />
-
-                <SelectBrewery setBrewery={setBrewery}/>
-                <SelectCategory setCategory={setCategory}/>
-
-                <TextInput className={text_input+" min-h-9"}                            // set country
-                    placeholder="Enter a country..."
-                    editable
-                    multiline
-                    numberOfLines={1}
-                    onChangeText={(event) => {handleInput(event, "country")}}/>
-
-                <TextInput className={text_input+" min-h-9"}                            // set percentage
-                    placeholder="Enter a percentage..."
-                    editable
-                    multiline
-                    numberOfLines={1}
-                    onChangeText={(event) => {handleInput(event, "percentage")}}/>
-
-                <TextInput className={text_input+" min-h-9 resize-y"}                            // set logo
-                    placeholder="Enter a logo..."
-                    editable
-                    multiline
-                    numberOfLines={1}
-                    onChangeText={(event) => {handleInput(event, "logo")}}/>
-
-                <TextInput className={text_input+" min-h-9 resize-y"}                            // set image
-                    placeholder="Enter an image..."
-                    editable
-                    multiline
-                    numberOfLines={1}
-                    onChangeText={(event) => {handleInput(event, "image")}}/>
-
-                <TextInput className={text_input+" min-h-24 resize-y"}                          // add tags
-                    placeholder="Add tags..."
-                    editable
-                    multiline
-                    numberOfLines={3}
-                    onChangeText={(event) => {handleInput(event, "tags")}}/>
-
-                <TouchableOpacity                                               // Submit Button
-                    onPress={() => { createBeer() }}
-                    className="flex-row justify-center bg-green-400 rounded-xl px-6 py-6 mx-4 mb-6 mt-3 shadow-sm hover:bg-green-300"
-                >
-                    <Text className="text-black text-lg font-bold">Submit Beer</Text>
-                </TouchableOpacity>                                          
-            </View>
-
+      {/* Main Beer Card Container */}
+      <View style={styles.beerCard} className="flex-row mb-6 p-4 shadow-lg">
+        <View style={styles.beerCardLeftColumn}>
+          <BeerCardTextBox
+            text={nameInput}
+            placeholder={"Name"}
+            className={text_box}
+          />
+          <BeerCardTextBox
+            text={category.name}
+            placeholder={"Category"}
+            className={text_box}
+          />
+          <BeerCardTextBox
+            text={brewery.name}
+            placeholder={"Brewery"}
+            className={text_box}
+          />
+          <BeerCardTextBox
+            text={countryInput}
+            placeholder={"Country"}
+            className={text_box}
+          />
+          <BeerCardTextBox
+            text={"tags"}
+            placeholder={"Tags"}
+            className={text_box}
+          />
         </View>
 
-    )
+        <View style={styles.beerCardRightColumn}>
+          <Image
+            source={require("../../../../assets/images/default-beer-image.png")}
+            style={styles.beer_img}
+            resizeMode="contain"
+          />
+        </View>
+      </View>
+
+      {/* BEER FORM INPUTS */}
+      <View className="text-white w-32 bg-stone-900 rounded-t-xl ml-6 p-2">
+        <Text>Create a Beer</Text>
+      </View>
+      <View className="w-full bg-stone-900/90 rounded-xl p-8 border border-amber-300/90 shadow-lg">
+        <TextInput
+          style={styles.textInputCommon}
+          placeholder="Enter the name of your beer..."
+          onChangeText={(event) => {
+            handleInput(event, "name");
+          }}
+        />
+
+        <SelectBrewery setBrewery={setBrewery} />
+        <SelectCategory setCategory={setCategory} />
+
+        <TextInput
+          style={styles.textInputCommon}
+          placeholder="Enter a country..."
+          editable
+          multiline
+          numberOfLines={1}
+          onChangeText={(event) => {
+            handleInput(event, "country");
+          }}
+        />
+
+        <TextInput
+          style={styles.textInputCommon}
+          placeholder="Enter a percentage..."
+          editable
+          multiline
+          numberOfLines={1}
+          onChangeText={(event) => {
+            handleInput(event, "percentage");
+          }}
+        />
+
+        <TextInput
+          style={styles.textInputCommon}
+          placeholder="Enter a logo..."
+          editable
+          multiline
+          numberOfLines={1}
+          onChangeText={(event) => {
+            handleInput(event, "logo");
+          }}
+        />
+
+        <TextInput
+          style={styles.textInputCommon}
+          placeholder="Enter an image..."
+          editable
+          multiline
+          numberOfLines={1}
+          onChangeText={(event) => {
+            handleInput(event, "image");
+          }}
+        />
+
+        <TextInput
+          style={[styles.textInputCommon, styles.tagsInput]}
+          placeholder="Add tags (comma separated)..."
+          editable
+          multiline
+          numberOfLines={3}
+          onChangeText={(event) => {
+            handleInput(event, "tags");
+          }}
+        />
+
+        {/* Submit Button */}
+        <TouchableOpacity
+          onPress={() => {
+            createBeer();
+          }}
+          className="flex-row justify-center bg-green-400 rounded-xl px-6 py-6 mx-4 mb-6 mt-3 shadow-sm hover:bg-green-300"
+        >
+          <Text className="text-black text-lg font-bold">Submit Beer</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  text_input: {
-    color: 'black',
-    backgroundColor: 'white',
+  container: {
+    flex: 1,
+  },
+
+  textInputCommon: {
+    color: "black",
+    backgroundColor: "white",
     borderRadius: 6,
-    margin: 8,
+    marginVertical: 8,
     padding: 8,
-    verticalAlign: "middle"
+    minHeight: 36,
+  },
+  tagsInput: {
+    minHeight: 100,
+    textAlignVertical: "top",
   },
   text_input_error: {
-    color: 'black',
-    backgroundColor: 'white',
-    border: 'solid',
-    borderColor: 'red',
+    color: "black",
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderColor: "red",
     borderRadius: 6,
     margin: 8,
-    padding: 6
+    padding: 6,
   },
   beer_img: {
-    width: '200px',
-    height: '200px',
-    aspectRatio: '1/1 auto',
-    borderRadius: '5%',
-  }
-})
+    width: screenWidth * 0.35,
+    height: screenWidth * 0.35,
+    borderRadius: 5,
+  },
+  beerCard: {
+    flexDirection: "row",
+    height: 232,
+    width: "100%",
+    backgroundColor: "rgba(139, 69, 19, 0.9)",
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: "rgba(251, 202, 38, 0.9)",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  beerCardLeftColumn: {
+    flex: 1,
+    marginRight: 10,
+    justifyContent: "space-around",
+  },
+  beerCardRightColumn: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
-export default BeerForm
+export default BeerForm;
